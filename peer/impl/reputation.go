176af4627d4f8ab56b89295a6c3f80dfbf68ee5e
jules20022000 <jules.maglione@epfl.ch>
2023-01-05 15:28:45 +0100

    reputation file

---
+++ peer/impl/reputation.go
@@ -0,0 +1,11 @@
+package impl
+
+import "go.dedis.ch/cs438/types"
+
+// A node has a reputation struct that corresponds to the number number of likes and number of dislikes he has
+// (it would be the total of its messages). Moreover he has the ability to like or dislike messages of other peers
+// and change their corresponding reputation score.
+
+func (n *node) InitReputationCheck(userID string, reputScore int) {
+	n.Consensus(userID, reputScore, types.Reputation)
+}
