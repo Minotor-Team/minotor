d819046a974054dbbf821f0a66a4d0222bfc5cfc
jules20022000 <jules.maglione@epfl.ch>
2023-01-05 15:17:50 +0100

    start consensus reputation

--- peer/impl/reputation.go
+++ peer/impl/reputation.go
@@ -1,14 +1,9 @@
 package impl
 
-// reputation !
-
 // A node has a reputation struct that corresponds to the number number of likes and number of dislikes he has
 // (it would be the total of its messages). Moreover he has the ability to like or dislike messages of other peers
 // and change their corresponding reputation score.
 
-func (n *node) Like(addr string) {
-}
-
-func (n *node) Dislike(addr string) {
-
+func (n *node) InitReputationCheck(value int) {
+	n.Tag()
 }
